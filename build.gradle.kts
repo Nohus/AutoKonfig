e0a3c1fac314dd8be51f24653a81ea38764b7864
Nohus <git@nohus.eu>
2021-10-06 21:57:57 +0100

    Updated JUnit version

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
     implementation(kotlin("stdlib", LibraryVersions.kotlin))
     implementation(kotlin("reflect", LibraryVersions.kotlin))
     implementation("com.typesafe:config:${LibraryVersions.typeSafeConfig}")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
 }
 
 tasks.withType<KotlinCompile> {
